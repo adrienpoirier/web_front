6943d917c32ecaa4b40c2f3bae68de51ab9c30b8
Adrien <adrien.poirier@oui-connect.fr>
2023-01-30 15:52:58 +0100

    Initial app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Login from './components/Login.vue'
+</script>
+
+<template>
+  
+  <Login />
+</template>
+
+
+<script lang="ts">
+export default {
+  
+  
+};
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <form @submit.prevent="login">
+        <div>
+          <label for="username">Nom d'utilisateur:</label>
+          <input id="username" v-model="username" />
+        </div>
+        <div>
+          <label for="password">Mot de passe:</label>
+          <input id="password" v-model="password" type="password" />
+        </div>
+        <button type="submit">Connexion</button>
+      </form>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  
+  export default defineComponent({
+    setup() {
+      const username = ref('');
+      const password = ref('');
+  
+      function login() {
+        // Traitement de la soumission du formulaire de connexion
+        console.log(`Username: ${username.value} Password: ${password.value}`);
+      }
+  
+      return {
+        username,
+        password,
+        login
+      };
+    }
+  });
+  </script>
+  
